c6e80126951b8c4ebfe9b0e49881b6252145b56a
rockfireredmoon <rockfire.redmoon@gmail.com>
2015-10-27 21:31:38 +0000

    Region chat now fully integrated to website

--- all/modules/eechat/js/eechat.js
+++ all/modules/eechat/js/eechat.js
@@ -8,8 +8,10 @@
   jQuery(document).ready(function($) {
 	  //var objDiv = document.getElementById("block-eechat-eechat-chat");
 	  //objDiv.scrollTop = objDiv.scrollHeight;
-	  var sh = $("#block-eechat-eewho-who")[0].scrollHeight;
-	  $("#block-eechat-eewho-chat").scrollTop(sh);
-  });
+	  /*var sh = $("#block-eechat-eechat-chat")[0].scrollHeight;
+	  $("#block-eechat-eechat-chat").scrollTop(sh);*/
+	  var sh = $(".chat-area")[0].scrollHeight;
+	  $(".chat-area").scrollTop(sh);
+  }); 
 
 })(jQuery);
--- all/modules/eechat/js/eewho.js
+++ all/modules/eechat/js/eewho.js
@@ -8,8 +8,8 @@
   jQuery(document).ready(function($) {
 	  //var objDiv = document.getElementById("block-eechat-eechat-chat");
 	  //objDiv.scrollTop = objDiv.scrollHeight;
-	  var sh = $("#block-eechat-eechat-chat")[0].scrollHeight;
-	  $("#block-eechat-eechat-chat").scrollTop(sh);
-  });
+	  var sh = $("#block-eechat-eewho-who")[0].scrollHeight;
+	  $("#block-eechat-eewho-who").scrollTop(sh);
+  }); 
 
 })(jQuery);
